07a2145ca8faedfca54c07224d1248db839dd072
Hanif-adedotun <hanif.adedotun@gmail.com>
2024-10-21 23:33:47 +0100

    Flask app integrated

---
+++ Flask/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Simple Flask App by Hanif Deployed on Google App Engine!"
+
+@app.route('/hello')
+def hello():
+    return render_template('index.html', author="Hanif Test")
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {
+        'message': 'This is some sample data',
+        'numbers': [1, 2, 3, 4, 5]
+    }
+    return jsonify(data)
+
+@app.route('/api/echo', methods=['POST'])
+def echo():
+    data = request.json
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
